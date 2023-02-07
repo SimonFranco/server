62034eaa5cd11c7568899b20d94355d8ecf609a5
Simon Franco <simonfranco0@gmail.com>
2023-02-07 10:54:37 -0800

    imported packages

---
+++ index.js
@@ -0,0 +1,12 @@
+import bodyParser from "body-parser";
+import  express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv"
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// configurations
