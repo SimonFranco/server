bc504fc3e5a611ce81f2ae9fd574f68552cc330b
Simon Franco <simonfranco0@gmail.com>
2023-02-12 12:33:50 -0800

    user routes created

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js"
+import userRoutes from "./"
 import {register } from "./controllers/auth.js"
 
 // configurations
