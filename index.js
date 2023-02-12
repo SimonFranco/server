820e32a294d9c91b41c9c2b9b7fa0a2bfcab7867
Simon Franco <simonfranco0@gmail.com>
2023-02-12 14:30:38 -0800

    post model created

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import authRoutes from "./routes/auth.js"
-import userRoutes from "./"
-import {register } from "./controllers/auth.js"
+import authRoutes from "./routes/auth"
+import userRoutes from "./routes/users"
+import postRoutes from "./routes/posts";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+
+
 
 // configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -40,10 +44,14 @@ const storage = multer.diskStorage({
 
 //  routes with files
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
 
 // routes
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+
 
 // mongoose setup
 const PORT = process.env.PORT || 6001;
