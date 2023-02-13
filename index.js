32f735ea7bace565525ae2e78fac978a1444cd73
Simon Franco <simonfranco0@gmail.com>
2023-02-12 16:14:23 -0800

    fixed importing issues

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import authRoutes from "./routes/auth"
-import userRoutes from "./routes/users"
-import postRoutes from "./routes/posts";
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middlewear/auth.js"
 
 
 
